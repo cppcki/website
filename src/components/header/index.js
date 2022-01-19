b05160d5ab5a6271380d0fbc4e6cc9a8c1a15c1f
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2022-01-18 22:49:25 -0800

    chore(*): convention fixes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,21 +1,20 @@
-import Ckilogo from '../../assets/images/ckilogo.png';
+import CKI_Logo from '../../assets/images/ckilogo.png';
 import './styles.css';
 
-export function Header ()
-{
-    return (
+export function Header() {
+  return (
     <div className='header'>
-        <div className='header-left'>
-            <img src={Ckilogo} width= '100' /> 
+      <div className='header-left'>
+        <img src={CKI_Logo} width="100" alt="cki logo" />
+      </div>
+      <div className='header-right'>
+        <div className="header-name">
+          circle k
         </div>
-        <div className='header-right'>
-            <div>
-                circle k
-            </div>
-            <div>
-                cal poly pomona
-            </div>
+        <div className="header-school">
+          cal poly pomona
         </div>
+      </div>
     </div>
-    );
+  );
 }
