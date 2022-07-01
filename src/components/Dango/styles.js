da8df9add00b98ef95fe6fde88a1e173faa3c15a
mady-dx <msarmiento1621@gmail.com>
2022-06-30 17:42:16 -0700

    It don't work. href is not being a bop

--- src/components/Dango/styles.js
+++ src/components/Dango/styles.js
@@ -1,5 +1,28 @@
+//Beta.reactjs.org
+
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: ${props => props.dericColor};
+  background-color: red;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  flex: 1;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Left = styled.div`
+  background-color: green;
+  width: 40%;
+  height: 400px;
+`;
+
+export const Right = styled.div`
+  background-color: blue;
+  flex: 1;
+  height: 300px;
 `;
