137cad87b0b5dd17d0b041ff558336d5973991eb
mady-dx <msarmiento1621@gmail.com>
2022-06-30 17:32:59 -0700

    Co-authored-by: deric <exelarios@users.noreply.github.com>

--- src/components/Anchor/index.js
+++ src/components/Anchor/index.js
@@ -6,6 +6,10 @@ import { ReactComponent as Chevron } from "../../assets/images/chevron.svg";
 
 export function Anchor(props) {
   const { text, href } = props;
+
+  // if (!href)
+  //   return new Error("Must provide href prop.");
+
   if (href.includes("http://", 0) || href.includes("https://", 0)) {
     return (
       <Container {...props}>
--- src/components/Dango/index.js
+++ src/components/Dango/index.js
@@ -1,9 +1,39 @@
-import { Container } from "./styles";
+import { 
+  Container, 
+  Content,
+  Left, 
+  Right
+} from "./styles";
+
+// import { Anchor } from "components";
+import { Join } from "pages";
+
+// Project directory
+// `cd ~/Documents/cki-website`
+
+// Update dependencies
+// `npm` || `npm install` # based on package.json
+
+// Run Server
+// `npm start`
 
 export function Dango(){
   return (
-    <Container dericColor = "blue">
-      deric sucks :)
+    <Container>
+      <Left>
+        <Content>
+          <h1 id="title">Become a Dango</h1>
+          <p id="desc">
+            Become part of one of the largest collegiate community service, leadership development, and freindship organization in the world
+          </p>
+        </Content>
+      </Left>
+      <Right>
+        <img id="owo" src="https://github.com/cppcki/website/blob/e2e776c2f24a2e210810b9af7ba08979c3c78382/src/assets/images/banquet.jpg" alt = "owo"/>
+        <img id="uwu" src="https://github.com/cppcki/website/blob/e2e776c2f24a2e210810b9af7ba08979c3c78382/src/assets/images/events/22.jpg" alt="totoro and random dude"/>
+        <img id="owu" src="https://raw.githubusercontent.com/cppcki/website/e2e776c2f24a2e210810b9af7ba08979c3c78382/src/assets/images/events/2.jpg" alt="the GOAT Derric"/>
+        
+      </Right> 
     </Container>
   );
 }
