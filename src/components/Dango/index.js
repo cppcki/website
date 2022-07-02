47560cb4e270ad1b34718e497a6dfba9719a8a1c
mady-dx <msarmiento1621@gmail.com>
2022-07-02 15:29:59 -0700

    finished making things work with a cute collage

--- src/components/Dango/index.js
+++ src/components/Dango/index.js
@@ -2,11 +2,11 @@ import {
   Container, 
   Content,
   Left, 
-  Right
+  Right,
 } from "./styles";
 
-// import { Anchor } from "components";
-import { Join } from "pages";
+import pic from "./collage1.png";
+import { Anchor } from "components/Anchor";
 
 // Project directory
 // `cd ~/Documents/cki-website`
@@ -24,15 +24,13 @@ export function Dango(){
         <Content>
           <h1 id="title">Become a Dango</h1>
           <p id="desc">
-            Become part of one of the largest collegiate community service, leadership development, and freindship organization in the world
+            Become part of one of the largest collegiate community service, leadership development, and freindship organization in the world!
           </p>
+          <Anchor text="Learn More" href="./About"/>
         </Content>
       </Left>
       <Right>
-        <img id="owo" src="https://github.com/cppcki/website/blob/e2e776c2f24a2e210810b9af7ba08979c3c78382/src/assets/images/banquet.jpg" alt = "owo"/>
-        <img id="uwu" src="https://github.com/cppcki/website/blob/e2e776c2f24a2e210810b9af7ba08979c3c78382/src/assets/images/events/22.jpg" alt="totoro and random dude"/>
-        <img id="owu" src="https://raw.githubusercontent.com/cppcki/website/e2e776c2f24a2e210810b9af7ba08979c3c78382/src/assets/images/events/2.jpg" alt="the GOAT Derric"/>
-        
+        <img src={ pic } width="600" alt="picture collage of Dango memories"/>
       </Right> 
     </Container>
   );
