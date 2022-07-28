79ad3350b0321d0d3e64e434bc09be7fac483353
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2022-07-28 00:49:40 -0700

    fix: file path, JSX structure, missing image alt and more

--- src/components/Tenants/index.js
+++ src/components/Tenants/index.js
@@ -2,30 +2,30 @@ import{
     Container, Content, Left, Middle, Right, Describe, 
 } from "./styles";
 
-import cool from "./CoolDango.png";
-import lit from "./LitDango.png";
-import plant from "./PlantDango.png";
+import Cool from "../../assets/images/CoolDango.png";
+import Lit from "../../assets/images/LitDango.png";
+import Plant from "../../assets/images/PlantDango.png";
 
 export function Tenants(){
     return(
       <Container>
         <Left>
             <Content>
-                <img src= { cool } width= "250"/>
+                <img src= {Cool} width="250" alt="Leadership"/>
                 <h1>Leadership</h1>
                 <Describe>Opportunities and chances for developing one's character and skills.</Describe>
             </Content>
         </Left>
         <Middle>
             <Content>
-                <img src= { plant } width= "215"/>
+                <img src= {Plant} width="215" alt="Service"/>
                 <h1>Service</h1>
                 <Describe>Make a difference within college, university campuses, and communities through acts of service.</Describe>
             </Content>
         </Middle>
         <Right>
             <Content>
-                <img src= { lit } width= "133"/>
+                <img src= {Lit} width="133" alt="Fellowship"/>
                 <h1>Fellowship</h1>
                 <Describe>Create a sense of community wihtin the club and enable members to have tight-knit bonds with one another.</Describe>
             </Content>
