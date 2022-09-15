4ce10ea771db4691358777140b7e91d76049f9db
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2022-09-14 21:24:55 -0700

    fix: remove dangos

--- src/components/Tenants/index.js
+++ src/components/Tenants/index.js
@@ -1,35 +1,31 @@
-import{
-    Container, Content, Left, Middle, Right, Describe, 
+import {
+  Container, 
+  Content,
+  Describe
 } from "./styles";
 
 import Cool from "../../assets/images/CoolDango.png";
 import Lit from "../../assets/images/LitDango.png";
 import Plant from "../../assets/images/PlantDango.png";
 
-export function Tenants(){
-    return(
-      <Container>
-        <Left>
-            <Content>
-                <img src= {Cool} width="250" alt="Leadership"/>
-                <h1>Leadership</h1>
-                <Describe>Opportunities and chances for developing one's character and skills.</Describe>
-            </Content>
-        </Left>
-        <Middle>
-            <Content>
-                <img src= {Plant} width="215" alt="Service"/>
-                <h1>Service</h1>
-                <Describe>Make a difference within college, university campuses, and communities through acts of service.</Describe>
-            </Content>
-        </Middle>
-        <Right>
-            <Content>
-                <img src= {Lit} width="133" alt="Fellowship"/>
-                <h1>Fellowship</h1>
-                <Describe>Create a sense of community wihtin the club and enable members to have tight-knit bonds with one another.</Describe>
-            </Content>
-        </Right>
-      </Container>
-    );
+export function Tenants() {
+  return (
+    <Container>
+      <Content>
+        <img src={Cool} width="250" alt="Leadership" />
+        <h1>Leadership</h1>
+        <Describe>Opportunities and chances for developing one's character and skills.</Describe>
+      </Content>
+      <Content>
+        <img src={Plant} width="215" alt="Service" />
+        <h1>Service</h1>
+        <Describe>Make a difference within college, university campuses, and communities through acts of service.</Describe>
+      </Content>
+      <Content>
+        <img src={Lit} width="133" alt="Fellowship" />
+        <h1>Fellowship</h1>
+        <Describe>Create a sense of community wihtin the club and enable members to have tight-knit bonds with one another.</Describe>
+      </Content>
+    </Container>
+  );
 }
--- src/components/Tenants/styles.js
+++ src/components/Tenants/styles.js
@@ -1,45 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    justify-content: center;
+    gap: 100px;
+    margin-bottom: 20px;
     display: flex;
     flex-wrap: wrap;
     width: 100%;
     flex: 1;
-    padding-top: 150px;
-    justify-content: center;
 `;
 
 export const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    
-`;
-
-export const Left = styled.div`
-  /* background-color: orange; */
+  display: flex;
   max-width: 300px;
-  height: 400px;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;  
-`;
-
-export const Middle = styled.div`
-  /* background-color: red; */
-  max-width: 300px;
-  height: 400px;
-  align-items: center;
-  margin-left: 5%;
-  justify-content: center;  
-`;
-
-export const Right = styled.div`
-  /* background-color: yellow; */
-  max-width: 300px;
-  height: 400px;
-  align-items: center;
-  margin-left: 5%;
-  justify-content: center;  
 `;
 
 export const Describe = styled.p`
