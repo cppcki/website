9f7979104311da101511cf691e372f0afb622d2e
Kestrel86 <valdezandrew20@gmail.com>
2022-10-10 21:17:49 -0700

    added temp comments on resource page

--- src/pages/Resources/index.js
+++ src/pages/Resources/index.js
@@ -1,9 +1,19 @@
 import { Navbar } from "../../components";
 
+/*
+Personal notes:
+  Make a button for each school sector'
+    make it push out
+    white background with black border (Temp)
+  Ask Gaelen for the links for the schools
+  get a logo for them, for now stick with temp until understand theme
+    
+*/
+
 export function Resources() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       imagine having resources? wat are we rich?
     </div>
   );
