0838202e536251fdbf85b17a42752b670b84e9d8
Kestrel86 <valdezandrew20@gmail.com>
2022-10-12 02:40:02 -0700

    Creation of boxes

--- src/pages/Resources/index.js
+++ src/pages/Resources/index.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import Navbar from "components/Navbar";
 
 /*
@@ -8,13 +9,49 @@ Personal notes:
   Ask Gaelen for the links for the schools
   get a logo for them, for now stick with temp until understand theme
     
+  Box for containing all contents, decide for either transparent or faded
+  Then can hold image and a bottom box containing the link to other club sites
+  
+  make two types of containers for row and column?
 */
 
 export function Resources() {
   return (
     <div>
       <Navbar />
-      imagine having resources? wat are we rich?
+      <Container>
+        <Box>
+          <BoxImage />
+          <BoxContents></BoxContents>
+        </Box>
+        <Box>
+          <BoxImage />
+          <BoxContents></BoxContents>
+        </Box>
+      </Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 5rem;
+`;
+
+const Box = styled.div`
+  background-color: darkgray;
+  border-radius: 5px;
+  color: "#eee";
+  min-height: 200px;
+  padding: 12;
+  width: 300px;
+`;
+
+const BoxImage = styled.img``;
+
+const BoxContents = styled.div``;
+
+const Boxbtn = styled.button``;
