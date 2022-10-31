a5d9e0dfe6c2ef8a831357b41acf447cb3905c87
Kestrel86 <valdezandrew20@gmail.com>
2022-10-31 00:12:03 -0700

    Set up boxes for sites

--- src/pages/Resources/index.js
+++ src/pages/Resources/index.js
@@ -1,48 +1,94 @@
 import styled from "styled-components";
 import Navbar from "components/Navbar";
-import Fullerton from "../../assets/images/fullerton.jpg";
-
-/*
-Personal notes:
-  Make a button for each school sector'
-    make it push out
-    white background with black border (Temp)
-  Ask Gaelen for the links for the schools
-  get a logo for them, for now stick with temp until understand theme
-    
-  Box for containing all contents, decide for either transparent or faded
-  Then can hold image and a bottom box containing the link to other club sites
-  
-  make two types of containers for row and column?
-
-  //each box is its own div with bounds for simpler BS
-*/
 
 export function Resources() {
   return (
     <div>
       <Navbar />
+      <Title>Foothill Division</Title>
       <Container>
         <Box>
-          <BoxImage src={Fullerton} alt="its fullerton" />
+          <img
+            src={
+              "https://sites.redlands.edu/globalassets/depts/gpe/images/ccc/pcc-placeholder.png"
+            }
+            alt="its PCC"
+          />
           <BoxContents>
-            <p>test</p>
+            <p>Pasadena City College</p>
+            <button>
+              <a href="https://lancerlife.pasadena.edu/organization/cki">
+                TEMP SITE
+              </a>
+            </button>
           </BoxContents>
         </Box>
         <Box>
-          <BoxImage src={Fullerton} alt="its fullerton" />
+          <img
+            src={
+              "https://eonreality.com/wp-content/uploads/2020/08/XR_LACC.jpg"
+            }
+            alt="its LCC"
+          />
           <BoxContents>
-            <p>test</p>
+            <p>Los Angeles City College</p>
+            <button>
+              <a href="">No Site</a>
+            </button>
           </BoxContents>
         </Box>
         <Box>
-          <BoxImage />
-          <BoxContents></BoxContents>
+          <img
+            src={
+              "https://colleges.claremont.edu/fivecadmissions/wp-content/uploads/sites/39/2019/06/aerial-claremont-colleges.jpg"
+            }
+            alt="its CC"
+          />
+          <BoxContents>
+            <p>Claremont Colleges</p>
+            <button>No Site</button>
+          </BoxContents>
+        </Box>
+        <Box>
+          <img
+            src={
+              "https://www.calstate.edu/attend/campuses/SiteAssets/campus-facts/campus-fact-data/campus-images/LOS%20ANGELES/los%20angeles-3.jpg"
+            }
+            alt="its CSULA"
+          />
+          <BoxContents>
+            <p>California State University Los Angeles</p>
+            <button>No Site</button>
+          </BoxContents>
         </Box>
       </Container>
+      <Title>District Information</Title>
       <Container>
         <Box>
-          <BoxImage />
+          <img
+            src={
+              "https://community.ucla.edu/images/orgs/studentorg/51-og.jpg?f446551bfd5a6b749543ff8a4dea947e"
+            }
+            alt="its fullerton"
+          />
+          <BoxContents>
+            <p>Circle K International Website</p>
+            <button>
+              <a href="http://www.cnhcirclek.org">Visit</a>
+            </button>
+          </BoxContents>
+        </Box>
+        <Box>
+          <img
+            src={"https://1000logos.net/wp-content/uploads/2016/11/fb-logo.jpg"}
+            alt="its fullerton"
+          />
+          <BoxContents>
+            <p>Circle K International Facebook</p>
+            <button>
+              <a href="https://www.facebook.com/cnhcirclek">Visit</a>
+            </button>
+          </BoxContents>
         </Box>
       </Container>
     </div>
@@ -59,29 +105,60 @@ const Container = styled.div`
   //@media, make flex direction column
 `;
 
+const Title = styled.h2`
+  font-family: "Permanent Marker", cursive;
+  align-items: center;
+  text-align: center;
+  text-transform: capitalize;
+  font-size: 35px;
+`;
+
 const Box = styled.div`
   color: white;
   border-radius: 10px;
-  color: "#eee";
   width: 300px;
   height: 200px;
   padding: 12;
   width: 300px;
   position: relative;
-  text-align: center;
-`;
-
-const BoxImage = styled.img`
-  width: 300px;
-  height: 200px;
-  border-radius: 10px;
+  text-align: left;
+  & > img {
+    width: 300px;
+    height: 200px;
+    border-radius: 10px;
+    filter: brightness(50%);
+  }
 `;
 
 const BoxContents = styled.div`
   position: absolute;
   top: 8px;
-`;
-
-const Boxbtn = styled.button`
-  cursor: pointer;
+  & > p {
+    position: absolute;
+    left: 10px;
+    font-size: 23px;
+    width: 200px;
+    font-family: "Permanent Marker";
+  }
+  & > button {
+    cursor: pointer;
+    height: 30px;
+    width: 120px;
+    border-radius: 5px;
+    color: black;
+    text-align: center;
+    position: absolute;
+    background-color: white;
+    top: 140px;
+    left: 10px;
+    & > a {
+      font-weight: bold;
+      text-decoration: none;
+      text-transform: capitalize;
+      text-align: center;
+    }
+    & > a:hover {
+      color: ${(props) => props.theme.hue.gold};
+    }
+  }
 `;
