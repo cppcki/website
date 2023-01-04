c26b480d284f1e76979c8ce4a3a29bfe85570709
Kestrel86 <valdezandrew20@gmail.com>
2023-01-03 22:33:15 -0800

    minor adjustments in code

--- src/pages/Resources/index.js
+++ src/pages/Resources/index.js
@@ -1,95 +1,109 @@
-import styled from "styled-components";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
+import {
+  Main,
+  Title,
+  Box,
+  BoxContents,
+  Container,
+  BoxButton,
+  ResourceImg,
+} from "./styles";
+
+import PCC from "../../assets/resources/PasadenaCityCollege.png";
+import LACC from "../../assets/resources/LACC.jpg";
+import ClaremontCollege from "../../assets/resources/ClaremontCollege.jpg";
+import CSULA from "../../assets/resources/CSULA.jpg";
+import MtSAC from "../../assets/resources/MtSAC.jpg";
+import ELAC from "../../assets/resources/ELAC.jpg";
+import facebook from "../../assets/resources/fb-logo.jpg";
+import cki from "../../assets/resources/CKI_logo.jpg";
+
+//desired plan.
+//Make boxes pop out and remove visit button
 
 export function Resources() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Main>
         <Title>Foothill Division</Title>
         <Container>
           <Box>
-            <img
-              src={
-                "https://sites.redlands.edu/globalassets/depts/gpe/images/ccc/pcc-placeholder.png"
-              }
-              alt="its PCC"
-            />
+            <ResourceImg src={PCC} alt="its CC" />
             <BoxContents>
               <p>Pasadena City College</p>
-              <a href="https://www.instagram.com/ckipcc/" target="_blank" rel="noreferrer">
-                <button>Visit</button>
+              <a
+                href="https://www.instagram.com/ckipcc/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BoxButton>Visit</BoxButton>
               </a>
             </BoxContents>
           </Box>
           <Box>
-            <img
-              src={
-                "https://eonreality.com/wp-content/uploads/2020/08/XR_LACC.jpg"
-              }
-              alt="its LCC"
-            />
+            <ResourceImg src={LACC} alt="its LCC" />
             <BoxContents>
               <p>Los Angeles City College</p>
-              <a href="https://www.instagram.com/lacccki/" target="_blank" rel="noreferrer">
-                <button>Visit</button>
+              <a
+                href="https://www.instagram.com/lacccki/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BoxButton>Visit</BoxButton>
               </a>
             </BoxContents>
           </Box>
           <Box>
-            <img
-              src={
-                "https://colleges.claremont.edu/fivecadmissions/wp-content/uploads/sites/39/2019/06/aerial-claremont-colleges.jpg"
-              }
-              alt="its CC"
-            />
+            <ResourceImg src={ClaremontCollege} alt="its CC" />
             <BoxContents>
               <p>Claremont Colleges</p>
-              <a href="http://claremontcki.weebly.com/" target="_blank" rel="noreferrer">
-                <button>Visit</button>
+              <a
+                href="http://claremontcki.weebly.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BoxButton>Visit</BoxButton>
               </a>
             </BoxContents>
           </Box>
           <Box>
-            <img
-              src={
-                "https://www.calstate.edu/attend/campuses/SiteAssets/campus-facts/campus-fact-data/campus-images/LOS%20ANGELES/los%20angeles-3.jpg"
-              }
-              alt="its CSULA"
-            />
+            <ResourceImg src={CSULA} alt="its CSULA" />
             <BoxContents>
               <p>California State University Los Angeles</p>
-              <a href="https://www.instagram.com/cki_csula" target="_blank" rel="noreferrer">
-                <button>Visit</button>
+              <a
+                href="https://www.instagram.com/cki_csula"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BoxButton>Visit</BoxButton>
               </a>
             </BoxContents>
           </Box>
           <Box>
-            <img
-              src={
-                "https://s3.amazonaws.com/photos.niche.com/0f6ab87f02d8e4074dc878fc8faf8286245fb338"
-              }
-              alt="its CSULA"
-            />
+            <ResourceImg src={MtSAC} alt="its MtSAC" />
             <BoxContents>
               <p>Mount San Antonio College</p>
-              <a href="https://www.instagram.com/mtsaccki/" target="_blank" rel="noreferrer">
-                <button>Visit</button>
+              <a
+                href="https://www.instagram.com/mtsaccki/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BoxButton>Visit</BoxButton>
               </a>
             </BoxContents>
           </Box>
           <Box>
-            <img
-              src={
-                "https://www.selacollab.org/wp-content/uploads/2019/03/ELAC-letter-Monterey-Park-campus.jpg"
-              }
-              alt="its CSULA"
-            />
+            <ResourceImg src={ELAC} alt="its ELAC" />
             <BoxContents>
               <p>East Los Angeles College</p>
-              <a href="https://www.instagram.com/elac_cki" target="_blank" rel="noreferrer">
-                <button>Visit</button>
+              <a
+                href="https://www.instagram.com/elac_cki"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BoxButton>Visit</BoxButton>
               </a>
             </BoxContents>
           </Box>
@@ -97,28 +111,28 @@ export function Resources() {
         <Title>District Information</Title>
         <Container>
           <Box>
-            <img
-              src={
-                "https://community.ucla.edu/images/orgs/studentorg/51-og.jpg?f446551bfd5a6b749543ff8a4dea947e"
-              }
-              alt="its fullerton"
-            />
+            <ResourceImg src={cki} alt="cki" />
             <BoxContents>
               <p>Circle K International Website</p>
-              <a href="http://www.cnhcirclek.org" target="_blank" rel="noreferrer">
-                <button>Visit</button>
+              <a
+                href="http://www.cnhcirclek.org"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BoxButton>Visit</BoxButton>
               </a>
             </BoxContents>
           </Box>
           <Box>
-            <img
-              src={"https://1000logos.net/wp-content/uploads/2016/11/fb-logo.jpg"}
-              alt="its fullerton"
-            />
+            <ResourceImg src={facebook} alt="its facebook" />
             <BoxContents>
               <p>Circle K International Facebook</p>
-              <a href="https://www.facebook.com/cnhcirclek" target="_blank" rel="noreferrer">
-                <button>Visit</button>
+              <a
+                href="https://www.facebook.com/cnhcirclek"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <BoxButton>Visit</BoxButton>
               </a>
             </BoxContents>
           </Box>
@@ -128,77 +142,3 @@ export function Resources() {
     </div>
   );
 }
-
-const Main = styled.div`
-  max-width: 1440px;
-  margin: auto;
-`;
-
-const Container = styled.div`
-  justify-content: center;
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  flex-wrap: wrap;
-  gap: 5rem;
-  padding: 5px;
-`;
-
-const Title = styled.h2`
-  font-family: "Permanent Marker", cursive;
-  align-items: center;
-  text-align: center;
-  text-transform: capitalize;
-  font-size: 35px;
-`;
-
-const Box = styled.div`
-  color: white;
-  border-radius: 10px;
-  width: 300px;
-  height: 200px;
-  padding: 12;
-  width: 300px;
-  position: relative;
-  text-align: left;
-  & > img {
-    width: 300px;
-    height: 200px;
-    border-radius: 10px;
-    filter: brightness(50%);
-  }
-`;
-
-const BoxContents = styled.div`
-  position: absolute;
-  top: 8px;
-  & > p {
-    position: absolute;
-    left: 10px;
-    font-size: 23px;
-    width: 200px;
-    font-family: "Permanent Marker";
-  }
-  & > a {
-    font-weight: bold;
-    text-decoration: none;
-    text-transform: capitalize;
-    text-align: center;
-    & > button {
-      cursor: pointer;
-      height: 30px;
-      border: none;
-      width: 120px;
-      border-radius: 5px;
-      color: black;
-      text-align: center;
-      position: absolute;
-      background-color: white;
-      top: 140px;
-      left: 10px;
-    }
-    & > button:hover {
-      color: ${(props) => props.theme.hue.gold};
-    }
-  }
-`;
---
+++ src/pages/Resources/styles.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  max-width: 1440px;
+  margin: auto;
+`;
+
+export const Container = styled.div`
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 5rem;
+  padding: 5px;
+`;
+
+export const Title = styled.h2`
+  font-family: "Permanent Marker", cursive;
+  align-items: center;
+  text-align: center;
+  text-transform: capitalize;
+  font-size: 35px;
+`;
+
+export const Box = styled.div`
+  color: white;
+  border-radius: 10px;
+  width: 300px;
+  height: 200px;
+  padding: 12;
+  width: 300px;
+  position: relative;
+  text-align: left;
+`;
+
+export const ResourceImg = styled.img`
+  width: 300px;
+  height: 200px;
+  border-radius: 10px;
+  filter: brightness(50%);
+`;
+
+export const BoxContents = styled.div`
+  position: absolute;
+  top: 8px;
+  & > p {
+    position: absolute;
+    left: 10px;
+    font-size: 23px;
+    width: 200px;
+    font-family: "Permanent Marker";
+  }
+  & > a {
+    font-weight: bold;
+    text-decoration: none;
+    text-transform: capitalize;
+    text-align: center;
+    & > button:hover {
+      color: ${(props) => props.theme.hue.gold};
+    }
+  }
+`;
+
+export const BoxButton = styled.button`
+  cursor: pointer;
+  height: 30px;
+  border: none;
+  width: 120px;
+  border-radius: 5px;
+  color: black;
+  text-align: center;
+  position: absolute;
+  background-color: white;
+  top: 140px;
+  left: 10px;
+`;
