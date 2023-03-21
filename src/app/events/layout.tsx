0c497bb01f9f330813879b5ec262e9248e9870a1
Kestrel86 <valdezandrew20@gmail.com>
2023-03-20 22:10:42 -0700

    created foundation for event card

--- src/app/events/layout.tsx
+++ src/app/events/layout.tsx
@@ -1,5 +1,8 @@
 import "@/app/globals.css";
 import Navbar from "@/components/Navbar";
+import EventCard from "@/components/EventCard";
+
+import Zavala from "@/assets/images/smile_zavala.jpg";
 
 export default function RootLayout({
   children,
@@ -11,6 +14,15 @@ export default function RootLayout({
       <body className="max-w-7xl m-auto px-8">
         <Navbar/>
         {children}
+        <EventCard
+          title="CKI Raid Night"
+          details="Eyes up Dangos! Come help your fellow members slay a king in Destiny 2 as we all try to navigate the dangers of King's Fall. "
+          thumbnail={Zavala}
+          tags="social"
+          date= "4/20/2023"
+          time="@ 7 p.m."
+          location= "Remotely @ Your Home"
+          />
       </body>
     </html>
   );
