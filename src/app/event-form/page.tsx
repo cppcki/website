5521cfb64bec72f675808ef90fb474f701137fe9
ExodiusLux <103782828+ExodiusLux@users.noreply.github.com>
2023-04-04 22:22:04 -0700

    Update page.tsx
    was able to get a object with all the data stored and created except for changing value of  checkbox for recurring

--- src/app/event-form/page.tsx
+++ src/app/event-form/page.tsx
@@ -1,36 +1,119 @@
 //copied from about
+'use client'
+import TextInput from "@/components/TextLabel";
+import React from "react";
+import { SyntheticEvent, useCallback, useState } from "react";
+
 function eventForm() {
+  type eventItem = {
+  title: string,
+  location: string,
+  thumbnail: string,
+  description: string,
+  date: string,
+  startTime: string,
+  endTime: string,
+  recurring: boolean,
+  points: number
+  };
+
+  const [events, setEvent] = React.useState<eventItem>({
+    title: "",
+    location: "",
+    thumbnail: "",
+    description: "",
+    date: "",
+    startTime: "",
+    endTime: "",
+    recurring: false,
+    points: 0
+  });
+
+  const handleInput = useCallback((event: SyntheticEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    setEvent({
+      ...events,
+      [target.name]: target.value
+    });
+  }, [events]);
+
+  function handleClick(event: any){
+    console.log(events);
+  }
+  
   return (
     <div>
       <h1>Add an Event</h1>
-      <p>Title</p>
-      <input type="text" placeholder="Title here"></input>
+      <TextInput
+        name="title"
+        label="Title"
+        type="text"
+        placeholder="Title Name Here"
+        value={events.title}
+        onChange={handleInput}
+      />
+      <TextInput
+        name="location"
+        label="Location"
+        type="text"
+        placeholder="Location "
+        value={events.location}
+        onChange={handleInput}
+      />
+      <TextInput
+        name="thumbnail"
+        label="Thumbnail"
+        type="url"
+        placeholder="Thumbnail URL"
+        value={events.thumbnail}
+        onChange={handleInput}
+      />
+      <TextInput
+        name="description"
+        label="Description"
+        type="text"
+        placeholder="Description"
+        value={events.description}
+        onChange={handleInput}
+      />
+      <TextInput
+        name="date"
+        label="Date"
+        type="date"
+        placeholder="Date"
+        value={events.date}
+        onChange={handleInput}
+      />
+      <TextInput
+        name="startTime"
+        label="Start Time"
+        type="time"
+        placeholder="Start Time"
+        value={events.startTime}
+        onChange={handleInput}
+      />
+      <TextInput
+        name="endTime"
+        label="End Time"
+        type="time"
+        placeholder="Date"
+        value={events.endTime}
+        onChange={handleInput}
+      />
 
-      <p>Location</p>
-      <input type="text" placeholder="Location here"></input>
-
-      <p>Thumbnail</p>
-      <input type="text" placeholder="Thumbnail here"></input>
-
-      <p>Description</p>
-      <input type="text" placeholder="Description here"></input>
-
-      <p>Date</p>
-      <input type="date"></input>
-
-      <p>Start Time</p>
-      <input type="time" placeholder="Date here"></input>
-
-      <p>End Time</p>
-      <input type="time" placeholder="Date here"></input>
-
-      <p>Recurring?</p>
-      <input type="checkbox"></input>
+      <h2>Recurring?</h2>
+      <input className="recurringCheck" name="recurring" value="true" type="checkbox"></input>
       
-      <p>Points</p>
-      <input type="number"></input>
+      <TextInput
+        name="points"
+        label="Points"
+        type="number"
+        placeholder="Points"
+        value={events.points}
+        onChange={handleInput}
+      />
 
-      <button>Create Event</button>
+      <button onClick={handleClick}>Create Event</button>
     </div>
     
   );
