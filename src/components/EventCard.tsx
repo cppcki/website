caefaaf177ac3e99754aa84bcef99ba30eaf5f1c
Kestrel86 <valdezandrew20@gmail.com>
2023-05-25 20:16:04 -0700

    adjusted to tailwind

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import type * as CSS from "csstype";
 import Image, { StaticImageData } from "next/image";
-import DateImage from "@/assets/images/Date.png";
 
 type EventCardProps = {
     title: string;
@@ -18,110 +16,21 @@ function EventCard(props: EventCardProps) {
     const { title, details, thumbnail, date, time, location,} = props;
 
     return (
-        <div style={Container}>
-            <Image style={testImage}
+        <div className="flex-col flex border-solid border-2 border-gray-300 rounded-lg w-72 min-w-0 min-h-fit bg-white text-left p-0 m-0 min-blend-normal">
+            <Image className="overflow-hidden rounded-t-lg h-72 w-72 min-h-0"
                 src={thumbnail}
                 alt="Alt image"
             />
-            <div style={Content}>
-                <div style={CardDate}>📅 {date} @ {time}</div>
-                <h2 style={CardTitle}>{title}</h2>
-                <p style={CardLocation}>{location}</p>
-                <p style={CardDetail}>{details}</p>
+            <div className="w-full p-2.5">
+                <div className="capitalize text-gray-600 text-sm font-medium">📅 {date} @ {time}</div>
+                <h2 className="capitalize text-blue-600	text-2xl font-bold mb-2">{title}</h2>
+                <p className="capitalize text-sm text-gray-600 font-medium">{location}</p>
+                <p className="text-sm capitalize font-medium">{details}</p>
             </div>
         </div>
     );
 }
 
 export default EventCard;
-
-const Container: CSS.Properties = {
-    flexDirection: "column",
-    display: 'flex',
-    border: 'solid 1px #ECECEC',
-    borderRadius: '10px',
-    width: '300px',
-    minWidth: "300px",
-    minHeight: '450px',
-    backgroundColor: "#FDFDFD",
-    textAlign: "left",
-    padding: 0,
-    margin: 0,
-    mixBlendMode: "normal"
-};
-
-const Content: CSS.Properties = {
-    width: '100%',
-    padding: '10px',
-};
-
-const CardDetail: CSS.Properties = {
-    marginBlockEnd: '10px',
-    marginBlockStart: '10px',
-    fontSize: "14px",
-    textTransform: "capitalize",
-    fontWeight: 500,
-};
-
-const CardTitle: CSS.Properties = {
-    textTransform: "capitalize",
-    color: '#004B85',
-    fontSize: "24px", 
-    fontWeight: 'bold',
-    marginBlockEnd: '10px',
-    marginBlockStart: '10px',
-    lineHeight: '25px',
-};
-
-const testImage: CSS.Properties = {
-    overflow: 'hidden',
-    borderTopLeftRadius: '10px',
-    borderTopRightRadius: '10px',
-    height: "300px",
-    minHeight: "300px",
-    width: '300px'
-};
-
-const CardDate: CSS.Properties = {
-  color: "#333333",
-  textTransform: "capitalize",
-  marginBlockStart: "10px",
-  marginBlockEnd: "10px",
-  fontSize: "13px",
-  lineHeight: "0px",
-  fontWeight: 500,
-}
-
-const DateIcon: CSS.Properties = {
-  fill: "#333333",
-  width: "12px",
-  marginRight: "5px"
-}
-
-const TagContainer: CSS.Properties = {
-  display: "flex",
-  marginBottom: "10px",
-  gap: "10px"
-}
-
-const Tag: CSS.Properties = {
-  borderRadius: "5px",
-  backgroundColor: "#004B85",
-  paddingTop: "2px",
-  paddingBottom: "2px",
-  paddingLeft: "5px",
-  paddingRight: "5px",
-  color: "#333333"
-}
-
-const CardLocation: CSS.Properties = {
-  color: "#333333",
-  textTransform: "capitalize",
-  fontSize: "15px",
-  marginBlockStart: "0",
-  marginBlockEnd: "10px",
-  lineHeight: '7px',
-  fontWeight: 500,
-}
 
 
--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,9 +1,9 @@
 import Head from "@/components/Head";
 import Main from "@/components/Main";
+import Navbar from "@/components/Navbar";
 
 import EventCard from "@/components/EventCard";
 import Zavala from "@/assets/images/smile_zavala.jpg";
-import type * as CSS from "csstype";
 
 
 function Events() {
@@ -11,8 +11,9 @@ function Events() {
     <>
       <Head title="Events"/>
       <Main>
+      <Navbar/>
         deez nuts events
-        <div style={Content}>
+        <div className="flex gap-5 overflow-scroll box-content">
         <EventCard
           title="CKI Raid Night"
           details="fum"
@@ -40,62 +41,10 @@ function Events() {
           time="7 p.m."
           location= "Remotely @ Your Home"
           />
-          <EventCard
-          title="CKI Raid Night"
-          details="Eyes up Dangos! Come help your fellow members slay a disciple in Destiny 2 as we all try to navigate the dangers of the Vow of the Disciple."
-          thumbnail={Zavala}
-          tags="social"
-          date= "4/29/2023"
-          time="7 p.m."
-          location= "Remotely @ Your Home"
-          /> 
-          <EventCard
-          title="CKI Raid Night"
-          details="Eyes up Dangos! Come help your fellow members slay ✨✨."
-          thumbnail={Zavala}
-          tags="social"
-          date= "5/1/2023"
-          time="7 p.m."
-          location= "Remotely @ Your Home"
-          /> 
-          <EventCard
-          title="CKI Raid Night"
-          details="Eyes up Dangos! Come help your fellow members slay a king in Destiny 2 as we all try to navigate the dangers of King's Fall."
-          thumbnail={Zavala}
-          tags="social"
-          date= "5/4/2023"
-          time="7 p.m."
-          location= "Remotely @ Your Home"
-          />
-          <EventCard
-          title="CKI Raid Night"
-          details="Eyes up Dangos! Come help your fellow members whack off."
-          thumbnail={Zavala}
-          tags="social"
-          date= "5/7/2023"
-          time="7 p.m."
-          location= "Remotely @ Your Home"
-          />
-          <EventCard
-          title="CKI Raid Night"
-          details="Eyes up Dangos! Come help your fellow members slay a king in Destiny 2 as we all try to navigate the dangers of King's Fall."
-          thumbnail={Zavala}
-          tags="social"
-          date= "5/10/2023"
-          time="7 p.m."
-          location= "Remotely @ Your Home"
-          />
           </div>
       </Main>
     </>
   );
 }
-
-const Content: CSS.Properties = {
-  display: "flex",
-  gap: "20px",
-  overflowX: "scroll",
-  boxSizing: "content-box"
-};
 
 export default Events;
