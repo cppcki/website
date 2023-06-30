636f389564cd2ea47997a360639281f8c4cb7c17
Kestrel86 <valdezandrew20@gmail.com>
2023-06-30 01:21:15 -0700

    adjusted spacing and indentation

--- src/components/Oaths.tsx
+++ src/components/Oaths.tsx
@@ -5,52 +5,51 @@ import LeadershipTenantImage from "@/assets/images/tenants/leadership.png";
 import FellowshipTenantImage from "@/assets/images/tenants/fellowship.png";
 
 type OathItemProps = {
-    title: string;
-    thumbnail: StaticImageData;
-    description: string;
+  title: string;
+  thumbnail: StaticImageData;
+  description: string;
 }
 
 function OathItem(props: OathItemProps) {
-    const { title, thumbnail, description } = props;
+  const { title, thumbnail, description } = props;
   
-  
-    return (
-      <div className="flex flex-col max-w-xs">
-        <Image
-          draggable={false}
-          className="aspect-square object-contain "
-          width={300}
-          height={300}
-          src={thumbnail}
-          alt={`${title}_tenant`}
-        />
-        <h2 className="text-2xl font-normal first-letter:capitalize">{title}</h2>
-        <p className="mt-2">{description}</p>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col max-w-xs">
+      <Image
+        draggable={false}
+        className="aspect-square object-contain "
+        width={300}
+        height={300}
+        src={thumbnail}
+        alt={`${title}_tenant`}
+      />
+      <h2 className="text-2xl font-normal first-letter:capitalize">{title}</h2>
+      <p className="mt-2">{description}</p>
+    </div>
+  );
+}
 
 
 function Oaths() {
-    return(
-        <div className="flex gap-x-2 justify-around my-10 flex-wrap">
-            <OathItem
-                title="service"
-                thumbnail={ServiceTenantImage}
-                description="Make a difference within college and university campuses and communities through acts of service"    
-            />
-            <OathItem
-                title="leadership"
-                thumbnail={LeadershipTenantImage}
-                description="Opportunities and chances for developing one's character and skills"    
-            />
-            <OathItem
-                title="fellowship"
-                thumbnail={FellowshipTenantImage}
-                description="Create a sense of community within the club and enable members to have tight-knit bonds with one-another"    
-            />
-        </div>
-    );
+  return (
+    <div className="flex gap-x-2 justify-around my-10 flex-wrap">
+      <OathItem
+        title="service"
+        thumbnail={ServiceTenantImage}
+        description="Make a difference within college and university campuses and communities through acts of service"    
+      />
+      <OathItem
+        title="leadership"
+        thumbnail={LeadershipTenantImage}
+        description="Opportunities and chances for developing one's character and skills"    
+      />
+      <OathItem
+        title="fellowship"
+        thumbnail={FellowshipTenantImage}
+        description="Create a sense of community within the club and enable members to have tight-knit bonds with one-another"    
+      />
+    </div>
+  );
 }
 
 export default Oaths;
