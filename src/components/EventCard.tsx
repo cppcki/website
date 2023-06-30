cb99c35cc5a6bdcf838fa74c135e82fe5aa4d3a4
Kestrel86 <valdezandrew20@gmail.com>
2023-06-30 01:43:30 -0700

    adjusted indentation in EventCard

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -16,18 +16,18 @@ function EventCard(props: EventCardProps) {
   const { title, details, thumbnail, date, time, location,} = props;
 
   return (
-      <div className="flex-col flex border-solid border-2 border-gray-300 rounded-lg w-72 min-w-0 min-h-fit bg-white text-left p-0 m-0 min-blend-normal">
-          <Image className="overflow-hidden rounded-t-lg h-72 w-72 min-h-0"
-              src={thumbnail}
-              alt="Alt image"
-          />
-          <div className="w-full p-2.5">
-              <div className="capitalize text-gray-600 text-sm font-medium">📅 {date} @ {time}</div>
-              <h2 className="capitalize text-blue-600	text-2xl font-bold mb-2">{title}</h2>
-              <p className="capitalize text-sm text-gray-600 font-medium">{location}</p>
-              <p className="text-sm capitalize font-medium">{details}</p>
-          </div>
+    <div className="flex-col flex border-solid border-2 border-gray-300 rounded-lg w-72 min-w-0 min-h-fit bg-white text-left p-0 m-0 min-blend-normal">
+      <Image className="overflow-hidden rounded-t-lg h-72 w-72 min-h-0"
+        src={thumbnail}
+        alt="Alt image"
+      />
+      <div className="w-full p-2.5">
+        <div className="capitalize text-gray-600 text-sm font-medium">📅 {date} @ {time}</div>
+        <h2 className="capitalize text-blue-600	text-2xl font-bold mb-2">{title}</h2>
+        <p className="capitalize text-sm text-gray-600 font-medium">{location}</p>
+        <p className="text-sm capitalize font-medium">{details}</p>
       </div>
+    </div>
   );
 }
 
