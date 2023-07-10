bfafae46b1b65736f81c0f29d1d87be9f1e4ac94
Kestrel86 <valdezandrew20@gmail.com>
2023-07-10 00:20:06 -0700

    edits to board text & img

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,27 +1,30 @@
 import Image, { StaticImageData } from "next/image";
 
 import ServiceTenantImage from "@/assets/images/tenants/service.png";
+import crabs from "@/assets/images/mrcrabs.jpg"
 
 
 type BoardItemProps = {
     thumbnail: StaticImageData;
     title: string;
+    description: string;
   }
   
 function BoardItem(props: BoardItemProps) {
-    const { title, thumbnail } = props;
+    const { title, thumbnail, description } = props;
 
     return (
         <div className="flex flex-col max-w-xs">
             <Image
                 draggable={false}
-                className="rounded-full border-solid border-black aspect-square object-contain border"
+                className="rounded-full border-solid border-black aspect-square object-fit border"
                 height={200}
                 width={200}
                 src={thumbnail}
                 alt={`${title}_board`}
             />
-            <h1 className="text-xl text-center pt-2">{title}</h1>
+            <h1 className="text-xl text-center pt-2 first-letter:capitalize font-normal">{title}</h1>
+            <p className="text-center pt-1 text-lg first-letter:capitalize">{description}</p>
         </div>
     );
 }
@@ -34,14 +37,17 @@ function Board() {
             <BoardItem
                 title="Anson"
                 thumbnail={ServiceTenantImage}
+                description="Board"
+            />
+            <BoardItem
+                title="Test"
+                thumbnail={crabs}
+                description="ahhhhh"
             />
             <BoardItem
                 title="Test"
                 thumbnail={ServiceTenantImage}
-            />
-            <BoardItem
-                title="Test"
-                thumbnail={ServiceTenantImage}
+                description="fuuck!"
             />
             </div>
         </div>
